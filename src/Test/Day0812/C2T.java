7fd65f8be2a24164e25f584cbabe786f3045b4ae
genkidanggo <969784728@qq.com>
2020-08-13 09:35:19 +0800

    Commit Message

--- src/Test/Day0812/C2T.java
+++ src/Test/Day0812/C2T.java
@@ -6,15 +6,18 @@ import java.util.Random;
 public class C2T {
     public static void main(String[] args) {
         int[][] n = new int[3][3];
+        int sum = 0;
         SecureRandom sr = new SecureRandom();
         for (int i = 0; i < n.length; i++){
             for (int j = 0; j < n[i].length; j++){
                 n[i][j] = sr.nextInt(10);
                 System.out.print(n[i][j]);
+                if (i == j)
+                    sum += n[i][i];
             }
             System.out.println();
         }
-        int sum = n[0][0] + n[1][1] + n[2][2];
+
         System.out.println("此矩阵对角线的和为：" +sum);
 
 
